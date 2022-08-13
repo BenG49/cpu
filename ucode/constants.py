5285e990036a477ce8d6b8fd02cbb0b326106af0
BenG49 <64862590+BenG49@users.noreply.github.com>
2022-08-13 16:26:35 -0400

    Restructure code to include inverted control lines

--- ucode/constants.py
+++ ucode/constants.py
@@ -1,27 +1,104 @@
 from enum import IntEnum
 
-'''
-CPU details
-'''
+# CPU details
 ADDR_BITS = 14
 OUT_BITS  = 16
-TCLK_MAX  = 6
 
-class Ctrl(IntEnum):
-	# Using a decoder for mutually exclusive ctrl lines to save space
-	CO, AO, EO, RO, HT, T0, SPO = range(1, 8)
-	FI, OI, II, RI, MAI, SU, BI, AI, J, CE, SPE, SPD = [1 << i for i in range(3, 15)]
-
-Instr = IntEnum('Instr', 'NOP LDA LDAI STA ADD ADDI SUB SUBI OUT HLT PHA PHC PLA RET JMP JZ JNZ JC JNC JS JNS JMPI JZI JNZI JCI JNCI JSI JNSI', start=0)
+'''
+Clock:     (HT)
+Counter:   (CO), CE, ~J
+B Reg:     ~BI
+A Reg:     ~AI, (AO)
+ALU:       (EO), SU
+MAR:       ~MAI
+RAM:       RI, (RO)
+Stack:     (SPO), ~SPE, ~SPD
+Flags:     ~FI
+Out Reg:   ~OI
+Instr Reg: ~II
+Control:   (T0)
+'''
 
 class BitSlice:
 	def __init__(self, start: int, len: int):
 		self.start = start
 		self.len = len
 
+	def mask(self) -> int:
+		return ((1 << self.len) - 1) << self.start
+
 	def get(self, val: int) -> int:
 		mask = (1 << self.len) - 1
 		return (val >> self.start) & mask
+
+	def apply(self, val_in: int, data: int) -> int:
+		val_in &= ~self.mask()
+		val_in |= data << self.start
+		return val_in
+
+class CtrlLine:
+	def apply(in_val: int, *args) -> int:
+		for l in args:
+			in_val = l.applyto(in_val)
+		return in_val
+
+	def bit(name: str, n: int, inv: bool=False):
+		return CtrlLine(name, BitSlice(n, 1), 0 if inv else 1)
+
+	def __init__(self, name: str, bits: BitSlice, val: int):
+		self.name = name
+		self.bits = bits
+		self.val = val
+
+	def applyto(self, in_val: int, enabled: bool=True):
+		if enabled:
+			return self.bits.apply(in_val, self.val)
+
+		# disabled value is either inverted or just zeroed out if bits.len > 1
+		if self.bits.len == 1:
+			return self.bits.apply(in_val, ~self.val)
+
+		return self.bits.apply(in_val, 0)
+
+	def isactive(self, val: int) -> bool:
+		return self.bits.get(val) == self.val
+
+	def __str__(self) -> str:
+		return self.name
+	
+	def __repr__(self) -> str:
+		return self.__str__()
+
+class Ctrl:
+	# Using a decoder for mutually exclusive ctrl lines to save space
+	CO = CtrlLine('CO', BitSlice(0, 3), 1)
+	AO = CtrlLine('AO', BitSlice(0, 3), 2)
+	EO = CtrlLine('EO', BitSlice(0, 3), 3)
+	RO = CtrlLine('RO', BitSlice(0, 3), 4)
+	HT = CtrlLine('HT', BitSlice(0, 3), 5)
+	T0 = CtrlLine('T0', BitSlice(0, 3), 6)
+	SPO = CtrlLine('SPO', BitSlice(0, 3), 7)
+
+	FI = CtrlLine.bit('FI', 3, True)
+	OI = CtrlLine.bit('OI', 4, True)
+	II = CtrlLine.bit('II', 5, True)
+	RI = CtrlLine.bit('RI', 6)
+	MAI = CtrlLine.bit('MAI', 7, True)
+	SU = CtrlLine.bit('SU', 8)
+	BI = CtrlLine.bit('BI', 9, True)
+	AI = CtrlLine.bit('AI', 10, True)
+	J =  CtrlLine.bit('J', 11, True)
+	CE = CtrlLine.bit('CE', 12)
+	SPE = CtrlLine.bit('SPE', 13, True)
+	SPD = CtrlLine.bit('SPD', 14, True)
+
+	ALL = (CO, AO, EO, RO, HT, T0, SPO, FI, OI, II, RI, MAI, SU, BI, AI, J, CE, SPE, SPD)
+	NONE = 0
+	for a in ALL:
+		NONE = a.applyto(NONE, False)
+	NONE &= 0x7FFF
+
+Instr = IntEnum('Instr', 'NOP LDA LDAI STA ADD ADDI SUB SUBI OUT HLT PHA PHC PLA RET JMP JZ JNZ JC JNC JS JNS JMPI JZI JNZI JCI JNCI JSI JNSI', start=0)
 
 INSTR_SLICE = BitSlice(0, 8)
 TCLK_SLICE  = BitSlice(8, 3)
--- ucode/ucode.py
+++ ucode/ucode.py
@@ -1,13 +1,14 @@
 from constants import *
 
+# TODO: everything is offset by 1, maybe not outputting NOP
+
 # readable output of which control lines are activated
 def ctrl_line_str(data: int) -> str:
 	out = ''
 
-	for c in Ctrl:
-		if (c < 8 and (data & 0b111) == c) or (c >= 8 and (data & c) == c):
-			out += str(Ctrl(c))[5:]
-			out += ' '
+	for c in Ctrl.ALL:
+		if c.isactive(data):
+			out += str(c) + ' '
 
 	return out
 
@@ -23,7 +24,7 @@ def read_ucode(f):
 
 			tclk = TCLK_SLICE.get(addr)
 
-			if tclk > TCLK_MAX or instr >= len(Instr): continue
+			if instr >= len(Instr): continue
 
 			cflag = CFLAG_SLICE.get(addr)
 			zflag = ZFLAG_SLICE.get(addr)
@@ -31,86 +32,126 @@ def read_ucode(f):
 
 			data = int(l.strip(), 16)
 
-			print(f'{addr:04x} {str(Instr(instr))[6:].rjust(4)}   {tclk}   {cflag}  {zflag}  {sflag}:   0x{data:04x}', ctrl_line_str(data))	
+			print(f'{addr:04X} {str(Instr(instr))[6:].rjust(4)}   {tclk}   {cflag}  {zflag}  {sflag}:   0x{data:04X}', ctrl_line_str(data))	
 
 # get data byte after instruction
-FETCH_VAL =  Ctrl.CO | Ctrl.MAI
+FETCH_VAL =  Ctrl.CO, Ctrl.MAI
 # move data byte after instruction into MAR
-FETCH_ADDR = Ctrl.RO | Ctrl.MAI | Ctrl.CE
+FETCH_ADDR = Ctrl.RO, Ctrl.MAI, Ctrl.CE
 
-# T0 = CO | MAI
-# T1 = CE | RO | II
-# T2 = CO | MAI (for non-implied instructions)
-# ucode for T2-6
+# T0 = CO, MAI
+# T1 = CE, RO, II
+# T2 = CO, MAI (for non-implied instructions)
 ucode = [
-	[0,                   0,                                       0,                                     0,                                       0],                # NOP
-	[FETCH_VAL,           FETCH_ADDR,                              Ctrl.RO | Ctrl.AI,                     Ctrl.T0,                                 0],                # LDA
-	[FETCH_VAL,           Ctrl.RO | Ctrl.AI | Ctrl.CE,             Ctrl.T0,                               0,                                       0],                # LDA imm
-	[FETCH_VAL,           FETCH_ADDR,                              Ctrl.AO | Ctrl.RI,                     Ctrl.T0,                                 0],                # STA
-	[FETCH_VAL,           FETCH_ADDR,                              Ctrl.RO | Ctrl.BI,                     Ctrl.EO | Ctrl.AI | Ctrl.FI,             0],                # ADD
-	[FETCH_VAL,           Ctrl.RO | Ctrl.BI | Ctrl.CE,             Ctrl.EO | Ctrl.AI | Ctrl.FI,           Ctrl.T0, 0,                              0],                # ADD imm
-	[FETCH_VAL,           FETCH_ADDR,                              Ctrl.RO | Ctrl.BI,                     Ctrl.EO | Ctrl.AI | Ctrl.SU | Ctrl.FI,   0],                # SUB
-	[FETCH_VAL,           Ctrl.RO | Ctrl.BI | Ctrl.CE,             Ctrl.EO | Ctrl.AI | Ctrl.SU | Ctrl.FI, Ctrl.T0, 0,                              0],                # SUB imm
-	[Ctrl.AO | Ctrl.OI,   Ctrl.T0,                                 0,                                     0,                                       0],                # OUT
-	[Ctrl.HT,             0,                                       0,                                     0,                                       0],                # HLT
-	[Ctrl.SPO | Ctrl.MAI, Ctrl.AO | Ctrl.RI | Ctrl.SPE | Ctrl.SPD, Ctrl.T0,                               0,                                       0],                # PHA
-	[Ctrl.SPE,            Ctrl.SPO | Ctrl.MAI,                     Ctrl.RO | Ctrl.AI,                     Ctrl.T0,                                 0],                # PLA
-	[FETCH_VAL,           Ctrl.RO | Ctrl.AI | Ctrl.CE,             Ctrl.SPO | Ctrl.MAI,                   Ctrl.CO | Ctrl.RI | Ctrl.SPE | Ctrl.SPD, Ctrl.AO | Ctrl.J], # CALL
-	[Ctrl.SPE,            Ctrl.SPO | Ctrl.MAI,                     Ctrl.RO | Ctrl.J,                      Ctrl.T0,                                 0],                # RET
-	[FETCH_VAL,           Ctrl.RO | Ctrl.J,                        Ctrl.T0,                               0,                                       0],                # JMP
-	[FETCH_VAL,           FETCH_ADDR,                              Ctrl.RO | Ctrl.J,                      Ctrl.T0,                                 0],                # JMP indirect
+	# NOP
+	[Ctrl.NONE] * 8,
+	# LDA
+	[FETCH_VAL,            FETCH_ADDR,                             (Ctrl.RO, Ctrl.AI)],
+	# LDA imm
+	[FETCH_VAL,            (Ctrl.RO, Ctrl.AI, Ctrl.CE)],
+	# STA
+	[FETCH_VAL,            FETCH_ADDR,                             (Ctrl.AO, Ctrl.RI)],
+	# ADD
+	[FETCH_VAL,            FETCH_ADDR,                             (Ctrl.RO, Ctrl.BI),                   (Ctrl.EO, Ctrl.AI, Ctrl.FI)],
+	# ADD imm
+	[FETCH_VAL,            (Ctrl.RO, Ctrl.BI, Ctrl.CE),            (Ctrl.EO, Ctrl.AI, Ctrl.FI)],
+	# SUB
+	[FETCH_VAL,            FETCH_ADDR,                             (Ctrl.RO, Ctrl.BI),                   (Ctrl.EO, Ctrl.AI, Ctrl.SU, Ctrl.FI)],
+	# SUB imm
+	[FETCH_VAL,            (Ctrl.RO, Ctrl.BI, Ctrl.CE),            (Ctrl.EO, Ctrl.AI, Ctrl.SU, Ctrl.FI)],
+	# OUT
+	[(Ctrl.AO, Ctrl.OI)],
+	# HLT
+	[[Ctrl.HT]],
+	# PHA
+	[(Ctrl.SPO, Ctrl.MAI), (Ctrl.AO, Ctrl.RI, Ctrl.SPE, Ctrl.SPD)],
+	# PLA
+	[[Ctrl.SPE],           (Ctrl.SPO, Ctrl.MAI),                   (Ctrl.RO, Ctrl.AI)],
+	# CALL
+	# TODO: use extra cycles to fix this
+	[FETCH_VAL,            (Ctrl.RO, Ctrl.AI, Ctrl.CE),            (Ctrl.SPO, Ctrl.MAI),                 (Ctrl.CO, Ctrl.RI, Ctrl.SPE, Ctrl.SPD, Ctrl.AO, Ctrl.J)],
+	# RET
+	[[Ctrl.SPE],           (Ctrl.SPO, Ctrl.MAI),                   (Ctrl.RO, Ctrl.J)],
+	# JMP
+	[FETCH_VAL,            (Ctrl.RO, Ctrl.J)],
+	# JMP indirect
+	[FETCH_VAL,            FETCH_ADDR,                             (Ctrl.RO, Ctrl.J)],
 ]
 
 def valid_jmp(instr: int, zero: bool, carry: bool, sign: bool) -> bool:
 	# offset indirect jumps back to normal jumps, have same conditions
 	if instr >= Instr.JMPI:
-		instr = instr - Instr.JMPI + Instr.JMP
+		instr -= Instr.JMPI - Instr.JMP
 
 	return (instr == Instr.JMP
 		or (instr == Instr.JZ  and zero)
 		or (instr == Instr.JNZ and not zero)
 		or (instr == Instr.JC  and carry)
 		or (instr == Instr.JNC and not carry)
-		or (instr == Instr.JS  and sign)
-		or (instr == Instr.JNS and not sign))
+		or (instr == Instr.JNS and not sign)
+        or (instr == Instr.JS  and sign))
 
 def get(addr: int) -> int:
+	def read_ucode_arr(instr, tclk):
+		if tclk - 2 >= len(ucode[instr]):
+			if tclk - 2 == len(ucode[instr]) and instr != Instr.HLT:
+				return CtrlLine.apply(Ctrl.NONE, Ctrl.T0)
+			
+			return Ctrl.NONE
+		
+		if instr == Instr.NOP:
+			return Ctrl.NONE
+
+		return CtrlLine.apply(Ctrl.NONE, *ucode[instr][tclk - 2])
+
 	instr = INSTR_SLICE.get(addr)
 	tclk  = TCLK_SLICE.get(addr)
 	zero  = ZFLAG_SLICE.get(addr)
 	carry = CFLAG_SLICE.get(addr)
 	sign  = SFLAG_SLICE.get(addr)
 
-	if tclk > TCLK_MAX or instr >= len(Instr): return 0
+	if instr > len(Instr): return Ctrl.NONE
 
 	# inc, set address
-	if tclk == 0: return Ctrl.CO | Ctrl.MAI
+	if tclk == 0: return CtrlLine.apply(Ctrl.NONE, Ctrl.CO, Ctrl.MAI)
 	# fetch instruction
-	if tclk == 1: return Ctrl.RO | Ctrl.II | Ctrl.CE
+	if tclk == 1: return CtrlLine.apply(Ctrl.NONE, Ctrl.RO, Ctrl.II, Ctrl.CE)
 
 	# jump instruction
 	if instr > Instr.JMP:
 		# jump condition is satisifed
 		if valid_jmp(instr, zero, carry, sign):
-			if instr >= Instr.JMP and instr < Instr.JMPI:
-				# microcode for imm jumps are all the same
-				return ucode[Instr.JMP][tclk - 2]
+			# immediate jump
+			if instr > Instr.JMP and instr < Instr.JMPI:
+				return read_ucode_arr(Instr.JMP, tclk)
+			# indirect jump
 			else:
-				# microcode for ind jumps are all the same
-				return ucode[Instr.JMP + 1][tclk - 2]
+				return read_ucode_arr(Instr.JMP + 1, tclk)
 
 		if tclk == 3:
-			return Ctrl.CE | Ctrl.T0
+			return CtrlLine.apply(Ctrl.NONE, Ctrl.CE, Ctrl.T0)
 
-		return 0
+		return Ctrl.NONE
 
-	return ucode[instr][tclk - 2]
+	return read_ucode_arr(instr, tclk)
 
-with open('ucode.hex', 'w') as file:
-	file.write('v2.0 raw\n')
+if __name__ == '__main__':
+	full = open('real/ucode-real.hex', 'w')
+	top = open('real/top-real.hex', 'w')
+	bot = open('real/bottom-real.hex', 'w')
+	files = (full, top, bot)
+
+	for f in files:
+		f.write('v2.0 raw\n')
 
 	for n in range(1 << ADDR_BITS):
-		file.write(f'{get(n):04x}')
-		file.write('\n')
+		i = get(n)
 
-read_ucode('ucode.hex')
+		full.write(f'{i:04x}\n')
+		top.write(f'{i >> 8:02x}\n')
+		bot.write(f'{i & 0xFF:02x}\n')
+	
+	for f in files:
+		f.close()
+
+	read_ucode('real/ucode-real.hex')
